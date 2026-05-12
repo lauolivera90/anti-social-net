76239f45fc52cd6f8579a5ecba225613819552e1
lauolivera90 <lautarobolivera098@gmail.com>
2026-05-12 01:53:56 -0300

    fix

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import '@/app/styles/index.css'
-import App from '@/App/router/App'
+import App from './App'
 import { AuthProvider } from '@/app/providers';
 import { BrowserRouter } from 'react-router-dom';
 
